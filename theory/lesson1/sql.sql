fdd0ecc2cb09b51b44dcb774dd36963962dd4d78
gmastrbit <gmastrbit@gmail.com>
2019-05-07 15:28:26 +0300

    lesson 1
    introduction

---
+++ theory/lesson1/sql.sql
@@ -0,0 +1,8 @@
+CREATE TABLE orders (
+	onum INT(11) NOT NULL,
+	amt DECIMAL(6,2) NOT NULL,
+	odate DATE NOT NULL,
+	cnum INT(11) NOT NULL,
+	snum INT(11) NOT NULL,
+	PRIMARY KEY(onum)
+) ENGINE=MyISAM;
